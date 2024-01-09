e907960c3ebeb07710930564818d31f044baad81
Alexander <alexander.moreno@utp.edu.co>
2024-01-09 10:19:39 -0500

    changes

--- 4. MVC and Despliegue API REST/MVC/models/mongodb/mongodb.js
+++ 4. MVC and Despliegue API REST/MVC/models/mongodb/mongodb.js
@@ -1,5 +1,5 @@
 import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb'
-const uri = 'mongodb+srv://nodejslearning:AmcDvg0306@cluster0.oq9gd.mongodb.net/?retryWrites=true&w=majority'
+const uri = 'mongodb+srv://nodejslearning:******@cluster0.oq9gd.mongodb.net/?retryWrites=true&w=majority'
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
